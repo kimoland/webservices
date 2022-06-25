e5c16cbf257b5b8148ea8fe35b4967dc5ebd1eaa
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-25 08:31:44 -0700

    Update api_1.php

--- proxy-tel/api_1.php
+++ proxy-tel/api_1.php
@@ -1,9 +1,9 @@
 <?php
 
 header('Content-type: application/json;');
-function getProxies()
+function getProxies($channel)
 {
-    $get = file_get_contents('https://t.me/s/GlypeX');
+    $get = file_get_contents('https://t.me/s/' . $channel);
     preg_match_all('#href="(.*?)" target="_blank" rel="noopener"#', $get, $prxs);
     preg_match_all('#class="tgme_widget_message_inline_button url_button" href="(.*?)"#', $get, $in_prxs);
     return $in_prxs[1] ?: $prxs[1];
@@ -13,15 +13,15 @@ function ExProxy($proxy)
     preg_match('#server=(.*?)&port=(.*?)&secret=(.*)#', $proxy, $info);
     return ['link' => $proxy, 'server' => $info[1], 'port' => $info[2], 'secret' => $info[3]];
 }
-if (!is_null($_GET['channel'])) {
-    $prxs = getProxies();
+if (!is_null($_GET['GlypeX'])) {
+    $prxs = getProxies($_GET['GlypeX']);
     if (!is_null($prxs)) {
         for ($p = count($prxs) - 1; $p >= 0; $p--) {
             $prxlist[] = ExProxy(html_entity_decode($prxs[$p]));
         }
         $poker = ['ok' => true, 'channel' => '@GlypeX', 'proxies' => $prxlist];
     } else {
-        $poker = ['ok' => false, 'channel' => '@GlypeX', 'message' => 'There is not any proxy in @'];
+        $poker = ['ok' => false, 'channel' => '@GlypeX', 'message' => 'There is not any proxy in @' . $_GET['GlypeX']];
     }
 } else {
     $poker = ['ok' => false, 'channel' => '@GlypeX', 'message' => 'I need channel param :|'];

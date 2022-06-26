0167f086bf4ab0e241c4485e440ea86205cd2131
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-26 02:32:12 -0700

    Update api_1.php

--- proxy-tel/api_1.php
+++ proxy-tel/api_1.php
@@ -1,26 +1,9 @@
 <?php
 
-function decrypt($ciphertext, $key)
-{
-    $method = "AES-256-CBC";
-    $iv = '';
-    $hash = substr($ivHashCiphertext, 16, 32);
-    return openssl_decrypt(base64_decode($ciphertext), $method, $key, OPENSSL_RAW_DATA, $iv);
-}
-
-$str = file_get_contents("https://iopsms.xyz/");
-$enc = json_decode($str, true)["encrypted"];
-$key = substr(hash("sha256", substr($enc, strlen($enc) - 16)), 0, 32);
-$str = substr($enc, 0, strlen($enc) - 16);
-$dec = decrypt(trim($str), trim($key));
-$dec = substr($dec, 16, strlen($dec));
-$json = json_decode($dec, true);
-
-for ($i = 0; $i < count($json); $i++) {
-    $ip = $json[$i]["ip"];
-    $port = $json[$i]["prt"];
-    $secret = $json[$i]["secret"];
-    $message .= "🌐Proxy: " . "https://t.me/proxy?server=$ip&port=$port&secret=$secret" . "\n\n";
+$prox = json_decode(file_get_contents("http://dl.dropboxusercontent.com/s/dn0mdwhk7mjwa0k/novingram_server.json"), true);
+for ($i = 0; $i < 8; $i++) {
+    $show = $prox["login"][$i];
+    $message .= "🌐Proxy: "."$show"."\n\n";
 }
 
 ?>

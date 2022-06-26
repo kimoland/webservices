d06c485570e24d128fbf3545248afa60b5bd0bb7
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-26 06:32:40 -0700

    Create api_2.php

---
+++ proxy-tel/api_2.php
@@ -0,0 +1,15 @@
+<?php
+
+$prox=json_decode(file_get_contents("https://mtpro.xyz/api/?type=mtproto"),true);
+for ($i = 0; $i < 6;$i++)
+{
+    $host = $prox[$i]["host"];
+    $port = $prox[$i]["port"];
+    $secret = $prox[$i]["secret"];
+    $ping = $prox[$i]["ping"];
+    $message .= "🌐Proxy: "."https://t.me/proxy?server=$host&port=$port&secret=$secret"."\n"."⚡️Ping: $ping"."\n\n";
+}
+
+?>
+
+<?php echo $message; ?>

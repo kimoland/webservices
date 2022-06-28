dcd360b5d27cd855080aab61e1bd60c49dbecc59
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-28 08:36:56 -0700

    Update api_2.php

--- proxy-tel/api_2.php
+++ proxy-tel/api_2.php
@@ -1,15 +1,13 @@
 <?php
 
 $prox=json_decode(file_get_contents("https://mtpro.xyz/api/?type=mtproto"),true);
-for ($i = 0; $i < 6;$i++)
+for ($i = 0; $i < 3;$i++)
 {
     $host = $prox[$i]["host"];
     $port = $prox[$i]["port"];
     $secret = $prox[$i]["secret"];
     $ping = $prox[$i]["ping"];
-    $message .= "🌐Proxy: "."https://t.me/proxy?server=$host&port=$port&secret=$secret"."\n"."⚡️Ping: $ping"."\n\n";
+    $message2 .= "🌐 Proxy: "."https://t.me/proxy?server=$host&port=$port&secret=$secret"."\n"."⚡️ Ping: $ping"."\n\n";
 }
 
 ?>
-
-<?php echo $message; ?>

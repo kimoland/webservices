3ce307b76775cdcdd3dc254a3a2b67846f80c3a1
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-28 08:37:23 -0700

    Update api_3.php

--- proxy-tel/api_3.php
+++ proxy-tel/api_3.php
@@ -1,13 +1,11 @@
 <?php
 
 $prox = json_decode(file_get_contents("https://shahrokh.xyz/mobogram/prxmgr/prxmgr.php"), true);
-for ($i = 0; $i < 8; $i++) {
+for ($i = 0; $i < 3; $i++) {
     $host = $prox[$i]["ip"];
     $port = $prox[$i]["prt"];
     $secret = $prox[$i]["secret"];
-    $message .= "🌐Proxy: "."https://t.me/proxy?server=$host&port=$port&secret=$secret"."\n\n";
+    $message3 .= "🌐 Proxy: "."https://t.me/proxy?server=$host&port=$port&secret=$secret"."\n\n";
 }
 
 ?>
-
-<?php echo $message; ?>
